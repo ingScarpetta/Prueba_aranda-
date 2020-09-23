1c1d7842e200391dd92acd4972849b391e1e1a12
Laura Camila Scarpetta Rodríguez <lcscarpettar@gmail.com>
2020-09-23 18:04:52 -0500

    se comprueba la configuracion de la libreria googletest

---
+++ PruebasUnitarias/prueba_ejemplo.cpp
@@ -0,0 +1,17 @@
+//
+// Created by lenovo on 23/9/2020.
+//
+
+#include "gtest/gtest.h"
+
+TEST(EjemploPrueba, NumeroUno) {
+    EXPECT_EQ(2, 1 + 1);
+}
+
+TEST(EjemploPrueba, NumeroDos) {
+    EXPECT_EQ(0, 1 - 1);
+}
+
+TEST(EjemploPruebaSuite2, NumeroUno) {
+    EXPECT_EQ(0, 1 - 1);
+}
