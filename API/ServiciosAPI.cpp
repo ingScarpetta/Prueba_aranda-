85d045fee7e448074b56a7c4e3a4b676189d8940
Laura Camila Scarpetta Rodríguez <lcscarpettar@gmail.com>
2020-09-25 14:15:50 -0500

    se configura  libreria REST y Sqlite3

--- API/ServiciosAPI.cpp
+++ API/ServiciosAPI.cpp
@@ -4,11 +4,27 @@
 
 #include "ServiciosAPI.h"
 #include <cpprest/http_client.h>
+#include <cpprest/json.h>
 
 using namespace web::http::client;
 
 API::ServiciosAPI::ServiciosAPI(const std::string &host, unsigned int puerto) {
-    http_client client(U("http://www.bing.com/"));
+    http_client client(U("http://localhost:8080/"));
+    web::http::http_request peticion;
+    peticion.set_method(web::http::methods::POST);
+    peticion.set_request_uri(U("crear"));
+    web::json::value entrada_json;
+    entrada_json[U("id")] = 12;
+    entrada_json[U("float")] = 12.45;
+    entrada_json[U("arreglo")] = web::json::value::array({2, 3, 4, 5});
+    utility::string_t nombre(U("Laura mi nombre"));
+    entrada_json[U("nombre")] = web::json::value(nombre, false);
+
+
+    peticion.set_body(entrada_json);
+    auto respuesta = client.request(peticion).get();
+    auto datos = respuesta.extract_json().get();
+
 }
 
 void API::ServiciosAPI::Subir() {
--- PruebasUnitarias/API/PruebaAPI.cpp
+++ PruebasUnitarias/API/PruebaAPI.cpp
@@ -3,13 +3,24 @@
 //
 
 #include "gtest/gtest.h"
+#include <cpprest/http_client.h>
+#include <cpprest/json.h>
+#include <iostream>
 
 #include "ServiciosAPI.h"
 
 using namespace API;
 
 TEST( PruebaApi , NumeroUno) {
-    ServiciosAPI a ("localHost",80);
-    a.Subir()
+    // web::json::value entrada_json;
+    // entrada_json[U("id")] = 12;
+    /*
+    entrada_json[U("float")] = 12.45;
+    entrada_json[U("arreglo")] = web::json::value::array({2, 3, 4, 5});
+    utility::string_t nombre(U("Laura mi nombre"));
+    entrada_json[U("nombre")] = web::json::value(nombre,
+                                                 false);
+    std::wcout << entrada_json << std::endl;
+     */
     EXPECT_EQ(1,1);
 }
