6d878fbd03489b4d2cc34c5ed9fbc9b5d4aaba1f
Laura Camila Scarpetta Rodríguez <lcscarpettar@gmail.com>
2020-09-26 01:43:11 -0500

    Se crea ejecutable principal con servidor REST API

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+#include "ServiciosAPI.h"
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    API::ServiciosAPI servidor(U("localhost"), 8080);
+    servidor.Subir();
+    std::cout << "Escuchando en http://localhost:8080/" << std::endl;
+    while (true);
     return 0;
 }
